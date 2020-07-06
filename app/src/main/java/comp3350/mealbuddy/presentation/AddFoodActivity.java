a30ee72df368ab76f55e01955eeaa26a6f682b35
Arvind Maan <arvindmaan1199@gmail.com>
2020-07-05 23:00:21 -0500

    created add food
    Co-Authored-By: Caden Chabot <chabotc@myumanitoba.ca>

--- app/src/main/java/comp3350/mealbuddy/presentation/AddFoodActivity.java
+++ app/src/main/java/comp3350/mealbuddy/presentation/AddFoodActivity.java
@@ -5,8 +5,20 @@ import comp3350.mealbuddy.R;
 import comp3350.mealbuddy.business.AccessAccount;
 import comp3350.mealbuddy.objects.Account;
 import comp3350.mealbuddy.objects.Day;
+import comp3350.mealbuddy.objects.Edible;
+import comp3350.mealbuddy.objects.Food;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AddFoodActivity extends AppCompatActivity {
 
@@ -14,10 +26,52 @@ public class AddFoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food);
+
         int dayOfYear = this.getIntent().getIntExtra("dayOfYear", -1);
-        String username = this.getIntent().getStringExtra("username");
+        final String username = this.getIntent().getStringExtra("username");
         AccessAccount accessAccount = new AccessAccount();
         Account account = accessAccount.getAccount(username);
-        Day day = accessAccount.getDay(account, dayOfYear);
+        final Day day = accessAccount.getDay(account, dayOfYear);
+
+        Button submit = (Button) findViewById(R.id.btnAddFood);
+        final EditText name = (EditText) findViewById(R.id.etFoodName);
+        final EditText labels = (EditText) findViewById(R.id.etLabels);
+        final EditText protein = (EditText) findViewById(R.id.etProtein);
+        final EditText fat = (EditText) findViewById(R.id.etFat);
+        final EditText carbs = (EditText) findViewById(R.id.etCarbs);
+        final EditText weight = (EditText) findViewById(R.id.etWeight);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.spnMealtimes);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<String> labelList = new ArrayList<>(Arrays.asList(labels.getText().toString().split(",")));
+                Food food = new Food(name.getText().toString(), labelList);
+                food.setWeight(Integer.parseInt(weight.getText().toString()));
+
+                food.addMacro(Edible.Macros.Protein, Integer.parseInt(protein.getText().toString()));
+                food.addMacro(Edible.Macros.Fat, Integer.parseInt(fat.getText().toString()));
+                food.addMacro(Edible.Macros.Carbohydrates, Integer.parseInt(carbs.getText().toString()));
+
+                Day.MealTime_t MT;
+                String spnString = spinner.getSelectedItem().toString();;
+                if (spnString.equals("Breakfast"))
+                    MT = Day.MealTime_t.BREAKFAST;
+                else if(spnString.equals("Lunch"))
+                    MT = Day.MealTime_t.LUNCH;
+                else if(spnString.equals("Dinner"))
+                    MT = Day.MealTime_t.DINNER;
+                else
+                    MT = Day.MealTime_t.SNACK;
+
+                day.addFood(MT, food);
+
+                Intent intent = new Intent(AddFoodActivity.this, TimelineActivity.class);
+                intent.putExtra("username", username);
+                AddFoodActivity.this.startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/comp3350/mealbuddy/presentation/TimelineActivity.java
+++ app/src/main/java/comp3350/mealbuddy/presentation/TimelineActivity.java
@@ -1,26 +1,24 @@
 package comp3350.mealbuddy.presentation;
 
-import comp3350.mealbuddy.R;
-import comp3350.mealbuddy.business.AccessAccount;
-import comp3350.mealbuddy.business.Calculator;
-import comp3350.mealbuddy.objects.Day;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.Calendar;
 import java.util.Date;
+
+import comp3350.mealbuddy.R;
+import comp3350.mealbuddy.business.AccessAccount;
+import comp3350.mealbuddy.business.Calculator;
+import comp3350.mealbuddy.objects.Day;
 
 
 public class TimelineActivity extends AppCompatActivity {
