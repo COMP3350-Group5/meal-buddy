264298b612523b3b2cc16bfde0b29e03a5641f89
Emmanuel <valetee@myumanitoba.ca>
2020-07-08 21:28:45 -0500

    Updated comments in AccessExercise.java

--- app/src/main/java/comp3350/mealbuddy/business/AccessExercise.java
+++ app/src/main/java/comp3350/mealbuddy/business/AccessExercise.java
@@ -1,3 +1,7 @@
+/****************************************
+ * AccessEdible
+ * Business Objects for managing exercises
+ ****************************************/
 package comp3350.mealbuddy.business;
 
 import comp3350.mealbuddy.application.Main;
@@ -9,19 +13,41 @@ public class AccessExercise {
     private DataAccessStub DAS;
     private DataAccessStub.DatabaseType databaseType;
 
+    /*
+     * Constructor
+     * Creates an object representing exercises in the database.
+     */
     public AccessExercise(){
         DAS = Services.openDAS(Main.DATABASE_NAME);
         databaseType = DataAccessStub.DatabaseType.EXERCISES;
     }
 
+    /*
+     * addEdible
+     * Add an exercise to the database.
+     * Parameters:
+     *     @param e - The exercise to add to the database.
+     */
     public void addExercise(Exercise e){
         DAS.addToDB(databaseType, e);
     }
 
+    /*
+     * updateEdible
+     * Update an exercise in the database.
+     * Parameters:
+     *     @param e - The exercise to update.
+     */
     public void updateExercise(Exercise e){
         DAS.updateDB(databaseType, e);
     }
 
+    /*
+     * removeEdible
+     * Remove an exercise from the database.
+     * Parameters:
+     *     @param e - The exercise to remove.
+     */
     public void removeExercise(Exercise e){
         DAS.removeFromDB(databaseType, e);
     }
