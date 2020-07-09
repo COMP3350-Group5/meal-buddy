5c98476eca8f7718492d3948ea3cd34c93b37e2a
Emmanuel <valetee@myumanitoba.ca>
2020-07-09 00:01:04 -0500

    Update comments for Exercise.java

--- app/src/main/java/comp3350/mealbuddy/objects/Exercise.java
+++ app/src/main/java/comp3350/mealbuddy/objects/Exercise.java
@@ -1,3 +1,8 @@
+/****************************************
+ * Exercise
+ * Class that contains an exercise amount and intensity
+ ****************************************/
+
 package comp3350.mealbuddy.objects;
 
 public class Exercise {
@@ -9,6 +14,12 @@ public class Exercise {
     public String name;
     public double duration;
 
+    /*
+     * Constructor
+     * Creates an Exercise
+     * Parameters:
+     *     @param name - The name of the exercise.
+     */
     public Exercise(String name) {
         this.name = name;
     }
