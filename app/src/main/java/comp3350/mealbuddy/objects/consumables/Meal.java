fcb7c67d57fcc7f4dd9f506df88fb0b729931418
Emmanuel <valetee@myumanitoba.ca>
2020-07-08 23:52:38 -0500

    Update comments in meal.java

--- app/src/main/java/comp3350/mealbuddy/objects/consumables/Meal.java
+++ app/src/main/java/comp3350/mealbuddy/objects/consumables/Meal.java
@@ -1,7 +1,8 @@
+/****************************************
+ * Meal
+ * An object containing a list of FoodIntPairs
+ ****************************************/
 package comp3350.mealbuddy.objects.consumables;
-
-import android.os.Build;
-import android.util.ArraySet;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -12,25 +13,49 @@ public class Meal extends Edible {
 
     public Set<FoodIntPair> ediblesInMeal;
 
+    /*
+     * Constructor
+     * Create a meal from a name and a list of labels
+     */
     public Meal(String name, List<String> labels) {
         super(name, labels);
         ediblesInMeal = new HashSet<>();
     }
 
+    /*
+     * Creates a meal with a name and creates an empty list of labels.
+     */
     public Meal(String name) {
         super(name, new ArrayList<String>());
         ediblesInMeal = new HashSet<>();
     }
 
-    public void updateFood(Food toUpdate, int quantity){
-        if (quantity <= 0) ediblesInMeal.remove(toUpdate);
-        else ediblesInMeal.add(new FoodIntPair(toUpdate, quantity));
+    /*
+     * updateFood
+     * Updates the quantity of a Food in the meal, or adds it if not already present.
+     * Parameters:
+     *     @param food - The food we are updating
+     *     @param quantity - The quantity of the food we are updating to
+     */
+    public void updateFood(Food food, int quantity){
+        if (quantity <= 0) ediblesInMeal.remove(food);
+        else ediblesInMeal.add(new FoodIntPair(food, quantity));
     }
 
+    /*
+     * removeFood
+     * Removes a food from the meal
+     * Parameters:
+     *     @param removeFood - The food to remove
+     */
     public void removeFood(Food toRemove) {
         ediblesInMeal.remove(toRemove);
     }
 
+    /*
+     * toString
+     * Override for the toString method.
+     */
     @Override
     public String toString(){
         String meal = name + ": ";
