662cb72c6ec4662b9da9d169f2f9ce0907d5d3f9
Caden Chabot <chabotc@myumanitoba.ca>
2020-07-09 19:41:57 -0500

    Added arrays for sex and activity level. Added inputs and spinners to sign up activity.

--- app/src/main/java/comp3350/mealbuddy/presentation/SignUpActivity.java
+++ app/src/main/java/comp3350/mealbuddy/presentation/SignUpActivity.java
@@ -5,11 +5,18 @@
 package comp3350.mealbuddy.presentation;
 
 import comp3350.mealbuddy.R;
+import comp3350.mealbuddy.business.AccessAccount;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private AccessAccount accessAccount = new AccessAccount();
     /*
      * onCreate
      * called when the activity is initially created
@@ -20,5 +27,21 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        EditText username = findViewById(R.id.etUsername);
+        EditText password = findViewById(R.id.etPassword);
+        EditText fullName = findViewById(R.id.etFullName);
+        EditText age = findViewById(R.id.etAge);
+        EditText height = findViewById(R.id.etHeight);
+        EditText weight = findViewById(R.id.etWeight);
+        Spinner activityLevel = findViewById(R.id.spnActivityLevel);
+        Spinner sex = findViewById(R.id.spnSex);
+        Button createAccount = findViewById(R.id.btnCreateAccount);
+
+        //link the on click listeners
+        createAccount.setOnClickListener((view) -> {
+            Intent intent = new Intent(SignUpActivity.this, TimelineActivity.class);
+            SignUpActivity.this.startActivity(intent);
+        });
     }
 }
