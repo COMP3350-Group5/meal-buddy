c8b180ef5058e4705b88dbafc844930dfa5b2931
aminlawal <allawal393@gmail.com>
2020-07-14 22:43:19 -0500

    Made the data structures in Meal private and provided accessor methods to add food or remove food from the hashSet(containing different foods and their quantities)

--- app/src/main/java/comp3350/mealbuddy/objects/consumables/Meal.java
+++ app/src/main/java/comp3350/mealbuddy/objects/consumables/Meal.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public class Meal extends Edible {
 
-    public Set<FoodIntPair> ediblesInMeal;
+    private Set<FoodIntPair> ediblesInMeal;
 
     public Meal(String name, List<String> labels) {
         super(name, labels);
@@ -31,6 +31,22 @@ public class Meal extends Edible {
 
     public boolean containsFood(Food food){
         return ediblesInMeal.contains( new FoodIntPair(food, 1) ); //returns true as long as food is the same
+    }
+
+    public void addFood(Food foodToAdd, int quantity) {
+        FoodIntPair foodIntPair = new FoodIntPair(foodToAdd, quantity);
+        if (!ediblesInMeal.contains(foodIntPair)) {
+            ediblesInMeal.add(foodIntPair);
+        } else {
+            updateFood(foodToAdd, quantity);
+        }
+    }
+
+    public void removeFood(Food foodToRemove, int quantity) {
+        FoodIntPair foodIntPair = new FoodIntPair(foodToRemove, quantity);
+        if (ediblesInMeal.contains(foodIntPair)) {
+            ediblesInMeal.remove(foodIntPair);
+        }
     }
 
     @Override
