1b87a3ed7eeafbcbdb67bcc18052ed9d7e5db832
Emmanuel <valetee@myumanitoba.ca>
2020-07-26 15:26:27 -0500

    Cleaned Code to allow compilation

---
+++ app/src/main/java/comp3350/mealbuddy/presentation/Messages.java
@@ -0,0 +1,26 @@
+package comp3350.mealbuddy.presentation;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import comp3350.mealbuddy.R;
+
+public class Messages {
+    public static void fatalError(final Activity owner, String message) {
+        AlertDialog alertDialog = new AlertDialog.Builder(owner).create();
+        alertDialog.setTitle("Fatal Error");
+        alertDialog.setMessage(message);
+        alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                owner.finish();
+            }
+        });
+        alertDialog.show();
+    }
+    public static void warning(Activity owner, String message) {
+        AlertDialog alertDialog = new AlertDialog.Builder(owner).create();
+        alertDialog.setTitle("Warning");
+        alertDialog.setMessage(message);
+        alertDialog.show();
+    }
+}
