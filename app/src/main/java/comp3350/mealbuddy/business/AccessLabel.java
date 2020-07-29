8090693990b1a592ce1eefab28dd6acb31d57b71
Arvind Maan <arvindmaan1199@gmail.com>
2020-07-29 17:31:38 -0500

    Added getEdible, removeEdible and updateEdible Tests

--- app/src/main/java/comp3350/mealbuddy/business/AccessLabel.java
+++ app/src/main/java/comp3350/mealbuddy/business/AccessLabel.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import comp3350.mealbuddy.application.Main;
 import comp3350.mealbuddy.application.Services;
+import comp3350.mealbuddy.objects.consumables.Edible;
 import comp3350.mealbuddy.persistence.DataAccess;
 
 public class AccessLabel {
@@ -24,6 +25,8 @@ public class AccessLabel {
      *     @param l - The label to be added.
      */
     public void addLabel(String l){
+        if (getLabel(l) != null)
+            return;
         DAS.addLabel(l);
     }
 
@@ -50,4 +53,20 @@ public class AccessLabel {
      */
     public List<String> getLabels() { return DAS.getLabels(); }
 
+    /*
+     * getEdible
+     * returns a edible from the string name
+     * Return:
+     *     the edible or null.
+     */
+    public String getLabel(String name){
+        if (name == null)
+            throw new NullPointerException("Label name can't be null");
+        List<String> l = getLabels();
+        for (String in : l){
+            if (in.equals(name))
+                return in;
+        }
+        return null;
+    }
 }
--- app/src/test/java/comp3350/mealbuddy/tests/business/AccessEdibleTest.java
+++ app/src/test/java/comp3350/mealbuddy/tests/business/AccessEdibleTest.java
@@ -5,30 +5,38 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import comp3350.mealbuddy.application.Main;
 import comp3350.mealbuddy.application.Services;
 import comp3350.mealbuddy.business.AccessEdible;
+import comp3350.mealbuddy.business.AccessLabel;
 import comp3350.mealbuddy.objects.consumables.Edible;
 import comp3350.mealbuddy.objects.consumables.Food;
 
 public class AccessEdibleTest {
 
     private static AccessEdible accessEdible;
+    private static AccessLabel accessLabel;
+
     private static Edible edible;
     private static final String EDIBLE_NAME = "testfood123123";
+    private static final List<String> LABELS = Arrays.asList("labeltest123");
 
 
     @Before
     public void initAccessEdibleTest(){
         Services.initializeDB(Main.DATABASE_NAME);
         accessEdible = new AccessEdible();
-        edible = new Food(EDIBLE_NAME, Arrays.asList("labeltest123"));
+        accessLabel = new AccessLabel();
+        for (String label : LABELS)
+            accessLabel.addLabel(label);
+        edible = new Food(EDIBLE_NAME, LABELS);
         accessEdible.addEdible(edible);
     }
 
     @Test
-    public void addEdible_nullEdible_dontAdd(){
+    public void addEdible_nullEdible_throwException(){
         try {
             accessEdible.addEdible(null);
             Assert.fail();
@@ -44,5 +52,63 @@ public class AccessEdibleTest {
             accessEdible.addEdible(edible);
         Assert.assertNotNull(accessEdible.getEdible(EDIBLE_NAME));
     }
-    
+
+    @Test
+    public void updateEdible_nullFoodString_throwException(){
+        try {
+            accessEdible.updateEdible(null, edible);
+            Assert.fail();
+        } catch (NullPointerException npe){
+            Assert.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void updateEdible_nullFood_throwException(){
+        try {
+            accessEdible.updateEdible(EDIBLE_NAME, null);
+            Assert.fail();
+        } catch (NullPointerException npe){
+            Assert.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void updateEdible_foodDoesntExist_throwException(){
+        try {
+            accessEdible.updateEdible("edible name that doesnt exist", edible);
+            Assert.fail();
+        } catch (IllegalArgumentException npe){
+            Assert.assertTrue(true);
+        }
+    }
+    @Test
+    public void removeEdible_nullFood_throwException(){
+        try {
+            accessEdible.removeEdible(null);
+            Assert.fail();
+        } catch (NullPointerException npe){
+            Assert.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void removeEdible_foodDoesntExist_throwException(){
+        try {
+            accessEdible.removeEdible("edible name that doesnt exist");
+            Assert.fail();
+        } catch (IllegalArgumentException npe){
+            Assert.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void getEdible_nullName_throwException(){
+        try {
+            accessEdible.getEdible(null);
+            Assert.fail();
+        } catch (NullPointerException npe){
+            Assert.assertTrue(true);
+        }
+    }
 }
