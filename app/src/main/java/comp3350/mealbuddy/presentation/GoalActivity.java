d41c97df34c3f7470c4c205203953d7ff9e52545
Emmanuel <valetee@myumanitoba.ca>
2020-07-28 20:18:38 -0500

    Add Calorie Goal Works

--- app/src/main/java/comp3350/mealbuddy/presentation/GoalActivity.java
+++ app/src/main/java/comp3350/mealbuddy/presentation/GoalActivity.java
@@ -8,7 +8,9 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +20,8 @@ import comp3350.mealbuddy.R;
 import comp3350.mealbuddy.business.AccessAccount;
 import comp3350.mealbuddy.business.Calculator;
 import comp3350.mealbuddy.objects.Day;
+import comp3350.mealbuddy.objects.goals.CalorieGoal;
+import comp3350.mealbuddy.objects.goals.Goal;
 
 public class GoalActivity extends AppCompatActivity {
 
@@ -37,6 +41,7 @@ public class GoalActivity extends AppCompatActivity {
         dialog = new Dialog(this);
 
         //get the passed values from the previous activity
+        accessAccount = new AccessAccount();
         int dayOfYear = this.getIntent().getIntExtra("dayOfYear", -1);
         final String username = this.getIntent().getStringExtra("username");
 
@@ -89,8 +94,39 @@ public class GoalActivity extends AppCompatActivity {
         dialog.setContentView(R.layout.pop_up_goal);
 
         Button btnContinue = dialog.findViewById(R.id.btnContinue);
+        Spinner spinner = dialog.findViewById(R.id.spnGoalType);
         btnContinue.setOnClickListener((view) -> {
-            //
+            String spnString = spinner.getSelectedItem().toString();
+            //find the right goal popup
+            if (spnString.equals("Calorie Goal"))
+                showCaloriePopUp(userName, dayOfYear);
+//            else if (spnString.equals("Label Goal"))
+//                showLabelPopUp();
+//            else if (spnString.equals("Macro Goal"))
+//                showMacroPopUp();
+//            else
+//                showMicroPopUp();
+        });
+
+        dialog.show();
+    }
+
+    public void showCaloriePopUp(String userName, int dayOfYear) {
+        dialog.setContentView(R.layout.add_calorie_goal);
+
+        EditText lowerBound = dialog.findViewById(R.id.lowerBound);
+        EditText upperBound = dialog.findViewById(R.id.upperBound);
+        Button btnContinue = dialog.findViewById(R.id.btnContinue);
+        btnContinue.setOnClickListener((view) -> {
+            Day day = accessAccount.getDay(userName, dayOfYear);
+            day.goals.add(new CalorieGoal(Integer.parseInt(lowerBound.getText().toString()), Integer.parseInt(upperBound.getText().toString())));
+            accessAccount.updateDay(userName, day);
+
+            //Go back to the goal activity
+            Intent intent = new Intent(GoalActivity.this, GoalActivity.class);
+            intent.putExtra("dayOfYear", dayOfYear);
+            intent.putExtra("username", userName);
+            GoalActivity.this.startActivity(intent);
         });
 
         dialog.show();
