dbad8ce6bf8af7bddd8f261224861c269686c529
Emmanuel <valetee@myumanitoba.ca>
2020-08-09 01:10:12 -0500

    Testing Creating an Account

--- app/src/androidTest/java/comp3350/mealbuddy/acceptance/HomeTests.java
+++ app/src/androidTest/java/comp3350/mealbuddy/acceptance/HomeTests.java
@@ -18,19 +18,20 @@ import static androidx.test.espresso.assertion.ViewAssertions.*;
 
 import static org.hamcrest.Matchers.not;
 
+import comp3350.mealbuddy.business.AccessAccount;
 import comp3350.mealbuddy.presentation.HomeActivity;
 import comp3350.mealbuddy.R;
+import comp3350.mealbuddy.presentation.SignUpActivity;
 import comp3350.mealbuddy.presentation.TimelineActivity;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 @LargeTest
 public class HomeTests {
 
-    private String testString1;
-    private String testString2;
-
     @Rule
     public ActivityTestRule<HomeActivity> main = new ActivityTestRule<>(HomeActivity.class);
+    public ActivityTestRule<SignUpActivity> main2 = new ActivityTestRule<>(SignUpActivity.class);
+
 
     @Test
     public void cantLogin() {
@@ -45,5 +46,25 @@ public class HomeTests {
         onView(withId(R.id.etUsername)).perform(typeText("admin"), ViewActions.closeSoftKeyboard());
         onView(withId(R.id.etPassword)).perform(typeText("group5"), ViewActions.closeSoftKeyboard());
         onView(withId(R.id.btnLogin)).perform(click());
+    }
+
+    @Test
+    public void canCreateAccount() {
+        AccessAccount access = new AccessAccount();
+
+        onView(withId(R.id.btnCreateAccount)).perform(click());
+        onView(withId(R.id.etFullName)).perform(typeText("testing tester"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.etUsername)).perform(typeText("tester"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.etPassword)).perform(typeText("tester123"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.etAge)).perform(typeText("100"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.etWeight)).perform(typeText("200"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.etHeight)).perform(typeText("200"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.spnSex)).perform(click());
+        onView(withText("Male")).perform(click());
+        onView(withId(R.id.spnActivityLevel)).perform(click());
+        onView(withText("Low")).perform(click());
+        onView(withId(R.id.btnCreateAccount)).perform(click());
+        access.removeAccount("tester");
+
     }
 }
