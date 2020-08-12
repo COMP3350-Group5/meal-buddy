a4e1d455bbddb5b593c1d4e73368555421bef6ed
Arvind Maan <arvindmaan1199@gmail.com>
2020-08-12 11:34:20 -0500

    created blank activity

---
+++ app/src/main/java/comp3350/mealbuddy/presentation/ViewStatsActivity.java
@@ -0,0 +1,16 @@
+package comp3350.mealbuddy.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp3350.mealbuddy.R;
+
+public class ViewStatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_stats);
+    }
+}
