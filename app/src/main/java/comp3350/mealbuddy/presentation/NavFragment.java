f74dec769edeb2e56d233fbb79ade5c15a86d76f
Caden Chabot <chabotc@myumanitoba.ca>
2020-08-12 14:56:47 -0500

    added fragment to every page it's needed on

--- app/src/main/java/comp3350/mealbuddy/presentation/NavFragment.java
+++ app/src/main/java/comp3350/mealbuddy/presentation/NavFragment.java
@@ -19,12 +19,10 @@ public class NavFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_SELECTED = "seclected";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String mSelected;
 
     public NavFragment() {
         // Required empty public constructor
@@ -34,16 +32,14 @@ public class NavFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param selected Parameter 1.
      * @return A new instance of fragment NavFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NavFragment newInstance(String param1, String param2) {
+    public static NavFragment newInstance(String selected) {
         NavFragment fragment = new NavFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_SELECTED, selected);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +48,7 @@ public class NavFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mSelected = getArguments().getString(ARG_SELECTED);
         }
     }
 
