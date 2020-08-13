642f5ae76f673aaee6dad78a860989f25d92a44a
Emmanuel <valetee@myumanitoba.ca>
2020-08-13 17:25:50 -0500

    WIP

--- app/src/androidTest/java/comp3350/mealbuddy/acceptance/HomeTests.java
+++ app/src/androidTest/java/comp3350/mealbuddy/acceptance/HomeTests.java
@@ -1,9 +1,21 @@
 package comp3350.mealbuddy.acceptance;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.*;
 import org.junit.runner.RunWith;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.CoordinatesProvider;
+import androidx.test.espresso.action.GeneralClickAction;
+import androidx.test.espresso.action.Press;
+import androidx.test.espresso.action.Tap;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.filters.LargeTest;
@@ -13,10 +25,12 @@ import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static org.hamcrest.Matchers.allOf;
 
 import comp3350.mealbuddy.business.AccessAccount;
 import comp3350.mealbuddy.presentation.HomeActivity;
 import comp3350.mealbuddy.R;
+import comp3350.mealbuddy.presentation.NavFragment;
 import comp3350.mealbuddy.presentation.SignUpActivity;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
@@ -24,7 +38,8 @@ import comp3350.mealbuddy.presentation.SignUpActivity;
 public class HomeTests {
 
     @Rule
-    public ActivityTestRule<HomeActivity> main = new ActivityTestRule<>(HomeActivity.class);
+    public ActivityTestRule<HomeActivity> homeTest = new ActivityTestRule<>(HomeActivity.class);
+
 
     @Test
     public void cantLogin() {
@@ -36,16 +51,20 @@ public class HomeTests {
     }
 
     @Test
-    public void canLogin() {
+    public void canLoginAndLogout() {
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.etUsername)).perform(typeText("admin"), ViewActions.closeSoftKeyboard());
         onView(withId(R.id.etPassword)).perform(typeText("group5"), ViewActions.closeSoftKeyboard());
         onView(withId(R.id.btnLogin)).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.bottom_navigation)).perform(clickXY(900, 0));
+        onView(withId(R.id.logOut)).perform(click());
     }
 
     @Test
     public void canCreateAccount() {
-        AccessAccount access = new AccessAccount();
+        AccessAccount accessAccount = new AccessAccount();
+        accessAccount.removeAccount("tester");
 
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.btnCreateAccount)).perform(click());
@@ -60,11 +79,21 @@ public class HomeTests {
         onView(withId(R.id.spnActivityLevel)).perform(click());
         onView(withText("Low")).perform(click());
         onView(withId(R.id.btnCreateAccount)).perform(click());
+        onView(withId(R.id.bottom_navigation)).perform(clickXY(900, 0));
 
-        access.removeAccount("tester");
+        onView(withText("Name: testing tester\n" +
+                "Username: tester\n" +
+                "Height: 200.0\n" +
+                "Sex: Male\n" +
+                "Age: 10\n" +
+                "Activity Level: Low\n" +
+                "Weight: 20.0")).check(matches(isDisplayed()));
+
+        accessAccount.removeAccount("tester");
     }
 
-    @Test public void emptyFields() {
+    @Test
+    public void testBadInputs() {
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.btnCreateAccount)).perform(click());
         Espresso.closeSoftKeyboard();
@@ -75,6 +104,37 @@ public class HomeTests {
         onView(withId(R.id.etAge)).check(matches(hasErrorText("Age is required")));
         onView(withId(R.id.etHeight)).check(matches(hasErrorText("Height is required")));
         onView(withId(R.id.etWeight)).check(matches(hasErrorText("Weight is required")));
+
+        onView(withId(R.id.etFullName)).perform(typeText("System Administrator"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.etUsername)).perform(typeText("admin"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.etPassword)).perform(typeText("group5"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.etAge)).perform(typeText("100"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.etWeight)).perform(typeText("200"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.etHeight)).perform(typeText("200"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.spnSex)).perform(click());
+        onView(withText("Male")).perform(click());
+        onView(withId(R.id.spnActivityLevel)).perform(click());
+        onView(withText("Low")).perform(click());
+        onView(withId(R.id.btnCreateAccount)).perform(click());
+        onView(withId(R.id.etUsername)).check(matches(hasErrorText("This username is taken select ")));
     }
 
+
+
+    public static ViewAction clickXY(final int x, final int y){
+        return new GeneralClickAction(
+                Tap.SINGLE,
+                view -> {
+
+                    final int[] screenPos = new int[2];
+                    view.getLocationOnScreen(screenPos);
+
+                    final float screenX = screenPos[0] + x;
+                    final float screenY = screenPos[1] + y;
+                    float[] coordinates = {screenX, screenY};
+
+                    return coordinates;
+                },
+                Press.FINGER);
+    }
 }
---
+++ app/src/androidTest/java/comp3350/mealbuddy/presentation/HomeActivityTest.java
@@ -0,0 +1,153 @@
+package comp3350.mealbuddy.presentation;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import comp3350.mealbuddy.R;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class HomeActivityTest {
+
+    @Rule
+    public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<>(HomeActivity.class);
+
+    @Test
+    public void homeActivityTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.etUsername),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.etUsername),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("admi"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.etUsername), withText("admi"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText3.perform(click());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.etUsername), withText("admi"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText4.perform(replaceText("admin"));
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.etUsername), withText("admin"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText5.perform(closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.etPassword),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText6.perform(replaceText("group5"), closeSoftKeyboard());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.btnLogin), withText("Sign In"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.action_account), withContentDescription("Account"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottom_navigation),
+                                        0),
+                                3),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.logOut), withText("Sign Out"),
+                        childAtPosition(
+                                allOf(withId(R.id.coordinatorLayout),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
